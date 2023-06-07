1321de4ba3d740fe70b5fa65d7ae9a7326d3aefa
Carlos-Tellez <124066370+Carlos-Tellez@users.noreply.github.com>
2023-06-07 08:08:59 -0500

    Primera entrega

---
+++ P_Entrega.js
@@ -0,0 +1,61 @@
+class Usuarios{
+
+    constructor(nom,apell){
+        this.nombre = nom,
+        this.apellido = apell,
+        this.libros = [],
+        this.mascotas = []
+    }
+
+    getFullName(){
+
+        let NomCompleto = `Mi nombre es: ${this.nombre} ${this.apellido}`
+        return NomCompleto
+    }
+
+    addMascota(mascota){
+
+        this.mascotas.push(mascota)
+    }
+
+    countMascotas(){
+
+        let numMasct = this.mascotas.length
+        return numMasct
+    }
+
+    addBook(titulo,autor){
+        let books = {
+            titulo: titulo,
+            autor: autor
+        }
+        this.libros.push(books)
+    }
+
+    getBookNames(){
+        return this.books.map(books => books.titulo)
+    }
+
+
+
+}
+
+const p1 = new  Usuarios("Carlos","Tellez")
+p1.addMascota("Gato")
+p1.addMascota("Perro")
+p1.addBook("Cronicas de una Muerte Anunciada", "Gabriel Garcia Marquez")
+p1.addBook("El Alquimista", "Paulo Coelho")
+console.log(p1.getFullName())
+console.log(p1.countMascotas())
+console.log(p1.getBookNames)
+
+const p2 = new  Usuarios("Santiago ","Torres")
+p2.addMascota("Gato")
+p2.addMascota("Gato")
+p2.addMascota("Pez")
+p2.addBook("100 años de soledad", "Gabriel Garcia Marquez")
+p2.addBook("Salon 8", "Yokoy Kenji")
+p2.addBook("Once Minutos", "Paulo Coelho")
+console.log(p2.getFullName())
+console.log(p2.countMascotas())
+console.log(p2.getBookNames)
